e4cea0ea10854ca4b499b45a7374b693db0af4ff
Alvtarino <Alvtarino@outlook.com>
2017-09-27 16:43:52 +0300

    rendering file

---
+++ rendering.py
@@ -0,0 +1,20 @@
+from flask import Flask,session,render_template, request,redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login')#, methods=['POST'])
+def login():
+    return render_template("login.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug = True)
