421ba1bcc3499d4d242d4428ffbd099fe88661b2
Alvtarino <Alvtarino@outlook.com>
2017-09-28 15:12:29 +0300

    new yummy recipes files

---
+++ recipe.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append('..')
+
+class Recipe(object):
+    def __init__(self, name, items):
+        self.name = name
+        self.description = ""
+        self.items = []
+
+    def __repr__(self):
+        return 'Recipes: ' + name + ', '.join(item for item in self.items)
